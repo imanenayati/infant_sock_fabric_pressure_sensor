f184f459e49c4229af31bbd13fabc7b399c440ea
Iman Enayati <57504586+imanenayati@users.noreply.github.com>
2021-09-23 20:55:26 -0700

    Update firmware.ino

--- firmware.ino
+++ firmware.ino
@@ -17,85 +17,41 @@ support.
 Last Updated: June 1, 2021
 */
 
-#define PIN 5 //Define pin for neopixels
-
-#define NUMPIXELS 1 //number of pixels that will be used
-
-#include <Filters.h>
-
-#include <Adafruit_NeoPixel.h>
-
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-//Defining Variables
-int delayval = 15;            // delay for .15 sec
-const int analogInPin1 = 10;  //pressure fabric for OUTSIDE, PIXEL 1
-int sensorHeelValue = 0;      //value from heel pressure sensor
-int sensorInsideValue = 0;    //value from inside pressure sensor
-int sensorOutValue = 0;       //value from outside pressure sensor
-int FilteredSensorHeelValue = 0;
-int FilteredSensorInsideValue = 0;
-int FilteredSensorOutValue = 0;
-
+  #include <Filters.h>
+  
+  int sensorOutValue = 0;       //value from outside pressure sensor
+  int FilteredSensorOutValue = 0;
+  
 void setup() {
-
-  Serial.begin(9600);
   
-  pixels.begin();             // This initializes the NeoPixel library.
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // initialize analog pin A0 as sensor input
+  pinMode(A0, INPUT_PULLUP);
+
+  // setup serial communication
   Serial.begin(9600);
-  pixels.setBrightness(255);
-  
-  pinMode(10, INPUT_PULLUP); //sensor pin
-
-  pinMode(5, OUTPUT);        //LED pin
-  pixels.show();
-
-//startup light sequence
-for (int i = 0; i <= 255; i++){
-    pixels.setPixelColor(0, pixels.Color(i,i,i));
-    pixels.show();
-    delay(20);
-  }
- delay(80);
-
-
 }
+
 
 void loop() {
 
+
   // read the analog in values from digital pin:
-  sensorOutValue = analogRead(analogInPin1);
+  sensorOutValue = analogRead(A0);
 
   //Set up for LowPass filter
   FilterOnePole lowpassFilter(LOWPASS,50);
 
   //Filter Heel sensor signal value due to high noise levels
-  lowpassFilter.input(sensorHeelValue);
+  lowpassFilter.input(sensorOutValue);
 
   //Create new variable to hold filtered signal
-  FilteredSensorHeelValue = lowpassFilter.output();
-             
-  //Print results to serial monitor:
-  Serial.print("sensor values = " );                       
-  Serial.print(sensorOutValue);
+  FilteredSensorOutValue = lowpassFilter.output();
 
-  
-/*If the sensor value is above a provided threshold (indicating inadequate pressure), 
-turn on the corresponding neopixel RED. If sensor value is not below threshold, turn 
-corresponding neopixel off*/
+  //Serial.println("Sensor value = " );
+  Serial.println(FilteredSensorOutValue);  
 
-if (sensorOutValue >= 60) {
-    pixels.setPixelColor(0, pixels.Color(255,0,0));
-    pixels.show();
-    Serial.println("NOT TRIGGERED");
-    delay(delayval);
-  }
-else{
-    pixels.setPixelColor(0, pixels.Color(0,0,0));
-    pixels.show();
-    Serial.println("TRIGGERED");
-    delay(delayval);
-  }
-
-
+  delay(10);
 }
